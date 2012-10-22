a15fb75a081bf9fc23888a69fd4cf6b0abaa03e0
Arash Badie Modiri <arashbm@gmail.com>
2012-10-22 21:12:12 +0330

    Post resource added

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,38 +1,48 @@
 class PostsController < ApplicationController
+  before_filter :authenticate_user!
+  before_filter :get_posts
   def index
-    @posts = Post.all
+    @posts = @visible_posts.all
     respond_with(@posts)
   end
 
   def show
-    @post = Post.find(params[:id])
+    @post = @visible_posts.find(params[:id])
     respond_with(@post)
   end
 
   def new
-    @post = Post.new
+    @post = @posts.new
     respond_with(@post)
   end
 
   def edit
-    @post = Post.find(params[:id])
+    @post = @editable_posts.find(params[:id])
   end
 
   def create
-    @post = Post.new(params[:post])
+    @post = @posts.new(params[:post])
     @post.save
     respond_with(@post)
   end
 
   def update
-    @post = Post.find(params[:id])
+    @post = @editable_posts.find(params[:id])
     @post.update_attributes(params[:post])
     respond_with(@post)
   end
 
   def destroy
-    @post = Post.find(params[:id])
+    @post = @editable_posts.find(params[:id])
     @post.destroy
     respond_with(@post)
+  end
+
+  private
+
+  def get_posts
+    @posts = current_user.posts
+    @visible_posts = current_user.visible_posts
+    @editable_posts = current_user.editable_posts
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,15 @@ class User < ActiveRecord::Base
   # :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me
-  # attr_accessible :title, :body
+  has_many :posts
+
+  # TODO: change to scope
+  def editable_posts
+    posts
+  end
+  def visible_posts
+    Post.scoped
+  end
 end
