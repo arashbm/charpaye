d2bab0d8b7862d4e1c46845fb2808c2771d2ad91
Arash Badie Modiri <arashbm@gmail.com>
2012-10-29 04:41:26 +0330

    removed precompile

--- config/application.rb
+++ config/application.rb
@@ -58,7 +58,5 @@ module Charpaye
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
-
-    config.initialize_on_precompile = false
   end
 end
