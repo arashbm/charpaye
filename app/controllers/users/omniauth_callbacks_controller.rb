fa9b06ba008aaf41fc7b7643a7869ecb9fb594c3
Arash Badie Modiri <arashbm@gmail.com>
2012-11-20 04:48:32 +0330

    redirects to edit_user_path on succesful signup

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,14 +1,18 @@
+# encoding: UTF-8
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def facebook
     # You need to implement the method below in your model (e.g. app/models/user.rb)
-    @user = User.find_for_facebook_oauth(request.env["omniauth.auth"], current_user)
+    @user = User.find_or_new_for_facebook_oauth(request.env["omniauth.auth"], current_user)
 
     if @user.persisted?
       sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
       set_flash_message(:notice, :success, :kind => "Facebook") if is_navigational_format?
     else
       session["devise.facebook_data"] = request.env["omniauth.auth"]
-      redirect_to edit_user_path(@user.id)
+      @user.save
+      sign_in @user
+      set_flash_message(:notice, :success, :kind => "Facebook") if is_navigational_format?
+      redirect_to edit_user_path(@user)
     end
   end
 end
